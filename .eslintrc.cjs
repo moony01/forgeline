cd1a35b671057149a3599f3d040880dda79f91eb
moony01 <mun01180@gmail.com>
2025-10-09 22:16:11 +0900

    chore: 린팅 및 테스트 환경 설정

---
+++ .eslintrc.cjs
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  ignorePatterns: ['**/dist/**', '**/node_modules/**'],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-recommended',
+    'prettier',
+    'plugin:vitest/recommended',
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'vue', 'vitest'],
+  rules: {
+    'vue/multi-word-component-names': 'off',
+  },
+  overrides: [
+    {
+      files: ['**/*.test.ts', '**/*.spec.ts'],
+    },
+    {
+      files: ['packages/forgeline-mcp-server/plugin/code.js'],
+      env: {
+        browser: true,
+      },
+      globals: {
+        figma: 'readonly',
+        __html__: 'readonly',
+      },
+    },
+  ],
+};
