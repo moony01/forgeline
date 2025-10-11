cf3dca2fedd371e48f1b4bd9541e6ab4ae5fac59
moony01 <mun01180@gmail.com>
2025-10-12 00:34:41 +0900

    docs: 배포 프로세스 업데이트 및 히스토리 기록 추가
    테스트 프레임워크 제거에 따라 deploy.md 프롬프트에서 test 단계를 제거. 이전 작업 내용을 history 문서로 기록.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,7 +11,6 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:vue/vue3-recommended',
     'prettier',
-    'plugin:vitest/recommended',
   ],
   parser: 'vue-eslint-parser',
   parserOptions: {
@@ -19,7 +18,7 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint', 'vue', 'vitest'],
+  plugins: ['@typescript-eslint', 'vue'],
   rules: {
     'vue/multi-word-component-names': 'off',
   },
